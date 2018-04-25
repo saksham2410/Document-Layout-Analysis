997dc98384528088070fa2e3307f1962328874ba
Saksham <gupta.10@iitj.ac.in>
2018-04-26 02:09:12 +0530

    Added Files

---
+++ cmake-build-debug/CMakeFiles/Document_Layout_Analysis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Document_Layout_Analysis.dir/main.cpp.o"
+  "Document_Layout_Analysis.pdb"
+  "Document_Layout_Analysis"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Document_Layout_Analysis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Document_Layout_Analysis.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Document_Layout_Analysis.
+# This may be replaced when dependencies are built.
